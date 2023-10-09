0b4058f91b9f6926831e457e1e894714b9960373
Owner PC <PC-Owner-t@outlook.jp>
2023-10-09 16:40:05 +0900

    プロジェクト ファイルを追加します。

---
+++ 2023_10_03_Task_1/2023_10_03_Task_1.cpp
@@ -0,0 +1,17 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int Sum(int num1, int num2);
+
+int main()
+{
+    int num1 = 0, num2 = 0;
+
+    printf("値1を入力 > ");
+    cin >> num1;
+    printf("値2を入力 > ");
+    cin >> num2;
+
+    printf("合計値は%dです。\n", Sum(num1, num2));
+}
---
+++ 2023_10_03_Task_1/Sum.cpp
@@ -0,0 +1,6 @@
+
+
+
+int Sum(int num1, int num2) {
+	return num1 + num2;
+}
